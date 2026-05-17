5253f320c637320634242e46a11060f7e6de5dca
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2026-05-17 14:07:21 -0600

    Added themed text input component.

---
+++ src/components/themed-text-input.tsx
@@ -0,0 +1,48 @@
+import { Spacing, ThemeColor } from "@/constants/theme";
+import { useTheme } from "@/hooks/use-theme";
+import React from "react";
+import { StyleSheet, TextInput, type TextInputProps } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export type ThemedTextInputProps = TextInputProps & {
+  type?: "default" | "subdued" | "highlighted";
+  themeColor?: ThemeColor;
+};
+
+export function ThemedTextInput({
+  placeholder,
+  style,
+  type = "default",
+  themeColor,
+  ...rest
+}: ThemedTextInputProps) {
+  const theme = useTheme();
+  const [text, onChangeText] = React.useState("");
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          style={[
+            { color: theme[themeColor ?? "text"] },
+            type === "default" && styles.default,
+          ]}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          value={text}
+          {...rest}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  default: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: Spacing.four,
+  },
+});
